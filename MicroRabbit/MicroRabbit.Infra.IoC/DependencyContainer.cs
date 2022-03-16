a8a5bfdc15a46e244e8f862c6e25b175ee261c91
Konstantin Khvan <Konstantin.K@debitsuccess.com>
2022-03-16 18:21:52 +1300

    Push Configuration of Api and dependencies

--- MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -1,20 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Services;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace MicroRabbit.Infra.IoC
+namespace MicroRabbit.Infra.IoC;
+
+public class DependencyContainer
 {
-    public class DependencyContainer
+    public static void RegisterServices(IServiceCollection services)
     {
-        public static void RegisterServices(IServiceCollection services)
-        {
-            //DomainBus
-            services.AddTransient<IEventBus, RabbitMQBus>();
-        }
+        //Domain Bus
+        services.AddTransient<IEventBus, RabbitMQBus>();
+
+        //Application Services
+        services.AddTransient<IAccountService, AccountService>();
+        
+        //Data
+        services.AddTransient<IAccountRepository, AccountRepository>();
+        services.AddTransient<BankingDbContext>();
     }
 }
