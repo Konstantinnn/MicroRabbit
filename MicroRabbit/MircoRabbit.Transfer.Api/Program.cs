88a13dc9f7de809e83b7d0c1f1b0d68932a6f82d
Konstantin Khvan <Konstantin.K@debitsuccess.com>
2022-03-19 22:50:56 +1300

    Transfer init project set up

---
+++ MicroRabbit/MircoRabbit.Transfer.Api/Program.cs
@@ -0,0 +1,42 @@
+
+using MediatR;
+using MicroRabbit.Infra.IoC;
+using MicroRabbit.Transfer.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var connectionString = builder.Configuration.GetConnectionString("TransferDbConnection");
+var services = builder.Services.AddDbContext<TransferDbContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
+
+DependencyContainer.RegisterServices(services);
+
+builder.Services.AddMediatR(typeof(Program));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
